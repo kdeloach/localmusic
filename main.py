78811bae94e57119f3732a5e78c2be3e3e53eaf2
Kevin DeLoach <kevin@SONIC.(none)>
2013-12-30 15:39:08 -0500

    parse command line options

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import sys
+import logging
+from localmusic import app
+
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
-from localmusic import app
+from tornado.options import parse_command_line
 
+parse_command_line()
 http_server = HTTPServer(WSGIContainer(app))
 http_server.listen(80)
 IOLoop.instance().start()
